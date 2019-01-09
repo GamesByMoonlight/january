20b753bc38d78031f1dd59a0473f21353a1a9fe6
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2019-01-08 23:19:02 -0600

    Issue13 (#22)
    * Added Canvas HUD Added Red Dot Cross Hair and Green Health HUD
    
    * testing Enemy and right arm fire
    
    * issue 13
    
    * Corrected camera and canvis for hud
    
    * also inserted white box
    
    * made white box bigger and resized enemies.. but not really part of this issue

---
+++ Assets/RightArm.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightArm : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+
+        }
+
+
+    }
+
+
+
+}
+
+
+
