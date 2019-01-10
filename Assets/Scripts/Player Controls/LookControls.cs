17702dd34fa56d0c7d4daff8ef63df6a8f526c90
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2019-01-10 13:51:34 -0600

    Added Camera control script (#32)
    * Camera is childed to a parent object so that the camera is offset slightly forward
     * Added LookControl.cs which controls the camera boom arm
    
    More file system tidying up

---
+++ Assets/Scripts/Player Controls/LookControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookControls : MonoBehaviour
+{
+
+    // Someone cool might want to make these variable in an options menu
+    public float horizontalSpeed = 2.0f;
+    public float verticalSpeed = 2.0f;
+    public bool invert = true;
+
+    private float pitch, yaw;
+
+    // Update is called once per frame
+    void Update()
+    {
+        pitch += (invert ? verticalSpeed : -verticalSpeed) * Input.GetAxis("Mouse Y");
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        yaw += horizontalSpeed * Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0);
+    }
+}
