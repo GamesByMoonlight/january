6d5c8d37afd58dc387c0f77f46e3add3a97eefc0
Lance Bailey <37199007+LanceBailey@users.noreply.github.com>
2019-01-14 11:12:12 -0600

    Put a script on shuriken so that it spins properly. (#47)
    Added a property so that the shuriken spin can easily be toggled (like, say, if we want it to "stick" in a bad guy/terrain).

--- Assets/Scripts/Shuriken/ShurikenSpin.cs
+++ Assets/Scripts/Shuriken/ShurikenSpin.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// =============================================
+///  Class to make the shuriken spin.
+///  
+///  Place on the model level of the shuriken prefab
+///  
+///  Should be easily modified later if we want the shuriken to "stick" on collision
+/// =============================================
+
 public class ShurikenSpin : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool IsRotating { get; set; } 
+
     void Start()
     {
-        
+        IsRotating = true;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (IsRotating)
+        {
+            transform.eulerAngles += new Vector3(0f, Time.deltaTime * 600, 0f);
+        }
     }
 }
