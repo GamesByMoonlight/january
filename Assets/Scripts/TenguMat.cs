085cde0b8afd2201357df75c00bfceafbcb893db
John <ninjaforhire8@yahoo.com>
2019-01-21 20:36:58 -0600

    fixed animation bugs, created death effect shader for tengu (#70)

---
+++ Assets/Scripts/TenguMat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TenguMat : MonoBehaviour
+{
+
+    Material mat;
+    SkinnedMeshRenderer mr;
+
+        // Start is called before the first frame update
+    void Start()
+        {
+            mr = GetComponent<SkinnedMeshRenderer>();
+            mat = new Material(mr.material);
+
+            mr.material = mat;
+
+        }
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
